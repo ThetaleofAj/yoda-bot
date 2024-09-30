084e4a343e2d32c847993e286206a18d427c01a1
whenceIT <whenceit@gmail.com>
2024-09-30 22:32:31 +0200

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+    
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-QXDGZ1ZH7E"></Script>
+<Script id="google-analytics">
+  { `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QXDGZ1ZH7E');
+       `
+  }
+</Script>
+      </head>
       <body
       
       >
